1ee81de14d23d4d6541d9581201bd97bed140a1e
PsychoMo <zymike09@hotmail.com>
2019-01-19 15:10:18 -0500

    Install typescript

--- Scripts/app.ts
+++ Scripts/app.ts
@@ -1,8 +1,11 @@
 // IIFE -- Immediately invoked function expression
-(function () {
-    var student;
+
+(function() {
+    let student: objects.Student;
+
     student = new objects.Student(20, "David", "D12345789");
     student.saysHello();
     student.studies();
+
 })();
-//# sourceMappingURL=app.js.map
+
